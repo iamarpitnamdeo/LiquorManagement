b4af5493110c300aab27f5a12147620f2f6974b9
arpitnamdeo <narpit2@gmail.com>
2023-06-04 21:44:20 +0530

    added initial code

---
+++ src/test/java/com/liquormanagement/LiquormanagementApplicationTests.java
@@ -0,0 +1,13 @@
+package com.liquormanagement;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LiquormanagementApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
